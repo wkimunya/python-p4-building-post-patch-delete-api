c9241b62bdf905fe8514473d43f19966fbda8721
wkimunya <136619749+wkimunya@users.noreply.github.com>
2023-12-12 10:32:44 +0300

    complete assignment

--- server/models.py
+++ server/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
+from app import app
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
